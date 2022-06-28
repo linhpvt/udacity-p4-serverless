c3e360daa12beadf412bd1bd357a6fa32996a1f8
linhpvt <linhpvt60@gmail.com>
2022-06-29 00:21:38 +0700

    fixing for deployment

--- backend/src/utils/index.ts
+++ backend/src/utils/index.ts
@@ -1,8 +1,5 @@
 import * as uuid from 'uuid';
-import { decode, verify } from 'jsonwebtoken';
-import { JwtPayload } from '../auth/JwtPayload';
 import { createLogger } from './logger';
-import { APIGatewayProxyEvent } from 'aws-lambda';
 
 const logger = createLogger('utils');
 
@@ -14,15 +11,6 @@ export const getTimestamp = () => {
 export const generateUUID = () => {
   return uuid.v4();
 }
-
-// export const decodeJwtToken = (jwtToken: string, defaultValue: any = null): JwtPayload => {
-//   try {
-//     return decode(jwtToken) as JwtPayload
-//   } catch (e) {
-//     logger.write(`tokenToJson: ${e.message}`);
-//   }
-//   return defaultValue;
-// }
 
 export const toJson = (str: string, defaultValue: any = '') => {
   try {
